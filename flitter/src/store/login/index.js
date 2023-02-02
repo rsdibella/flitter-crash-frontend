75c479069287f412a2744bc3c16fa1112c74859e
ceciaguicam <75157954+ceciaguicam@users.noreply.github.com>
2023-02-02 18:54:05 +0100

    Creada vista simple de login. Al meter mail y password los valores introducidos se guardan en el objeto "credentials".

--- flitter/src/store/login/index.js
+++ flitter/src/store/login/index.js
@@ -1,11 +1,13 @@
 
 import state from "./state";
 import getters from "./getters";
+import mutations from "./mutations";
 
 const loginModule = {
     namespaced: true,
     state,
-    getters
+    getters,
+    mutations
 }
 
 export default loginModule
---
+++ flitter/src/views/LoginView.vue
@@ -0,0 +1,65 @@
+<template>
+    <br />
+    <div>
+      <h1>Inicio de sesión</h1>
+    </div>
+    <br />
+    <div class="login-form">
+      <form>
+        <div>
+          <input type="email" placeholder="E-mail" v-model="credentials.email" required>
+        </div>
+        <br />
+        <div>
+          <input type="password" placeholder="Password" v-model="credentials.password" required>
+        </div> 
+        <br />
+        <div id="lower">
+            <input type="submit" value="Login" @click="showCredentials">  
+        </div>
+      </form>
+    </div>
+ </template>
+  
+<script>
+
+  import { defineComponent, ref } from 'vue';
+  
+  export default defineComponent({
+    name: 'loginView',
+    components: {
+      
+    },
+  
+  setup() {
+
+    const email = ref(null)
+    const password = ref(null)
+
+    const credentials = ref({
+      email: null,
+      password: null
+    })
+
+    const showCredentials = () => console.log(credentials)
+
+    return {
+      email,
+      password,
+      credentials,
+      showCredentials
+    }       
+  }
+});
+
+  
+</script>
+
+<style scoped>
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
